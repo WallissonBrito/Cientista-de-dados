17e36b6a9e3628c4c6db1c32535f12a7664f4959
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2022-12-07 13:48:47 -0300

    Data Manipulation with Pandas

---
+++ Data Manipulation with Pandas/Inspecting_DataFrame.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+""" Quando você obtém um novo DataFrame para trabalhar, a primeira coisa que você precisa fazer é explorá-lo e ver 
+o que ele contém. Existem vários métodos e atributos úteis para isso. """
+
+
+
+
+data = pd.read_csv("homelessness.csv")
+print(data)
+
